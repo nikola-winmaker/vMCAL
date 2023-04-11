9043b1b392847f769fd2a47b8fb5cf71529f806c
Nikola <nikola.stojkov@continental-corporation.com>
2023-04-11 12:56:10 +0200

    Add main python script.

---
+++ test_env/main.py
@@ -0,0 +1,64 @@
+import sys
+import ctypes
+import threading
+
+sys.path.insert(0, 'src_gen')
+from vFLS import vFlash
+
+class AutosarSIL:
+    def __init__(self, dll_path):
+        # Load the DLL and import the functions
+        self.app_dll = ctypes.CDLL(dll_path)
+        # instantiate vFlash
+        self.vFLS = vFlash(self.app_dll)
+
+    def init_dll_appl(self):
+        # msg receiving
+        self.autosar_thread = threading.Thread(target=self.run_dll_main)
+        self.autosar_thread.daemon = True
+        self.autosar_thread.start()
+
+        # initialize callbacks
+        self.vFLS.set_callbacks()
+
+    def run_dll_main(self):
+        print("entered DLL main")
+        dll_main_func = self.app_dll.main
+        dll_main_func.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)]
+        dll_main_func.restype = ctypes.c_int
+
+        while True:
+
+            # callback = self.app_dll.set_vFls_Write_callback
+        
+            # callback.argtypes = [ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_int)]
+            # callback_func = ctypes.CFUNCTYPE(None, ctypes.c_int,  ctypes.c_int)(self.vFLS.vFls_Write_callback)
+
+            # callback(callback_func)
+
+            #self.vFLS.set_vFls_Write_callback()
+
+            # Run the main function in a loop
+            argc = 1
+            argv = [None, None]
+            argv_p = ctypes.c_char_p(argv[0])
+            result = dll_main_func(argc, ctypes.pointer(argv_p))
+            print("DLL main returned", result)
+
+    def start_simulation(self):
+        start_func = self.app_dll.start_application
+        start_func.argtypes = [ctypes.CFUNCTYPE(None)]
+        func = ctypes.CFUNCTYPE(None)(start_func)
+        start_func(func)
+
+dll_path = 'd:\\DSUsers\\uic74928\\Trainings\\Autosar_training\\tresos_Wincore\\workspace\\demoRte\\util\\\c_app.dll'
+as_sil = AutosarSIL(dll_path)
+as_sil.init_dll_appl()
+
+import time
+time.sleep(1)
+as_sil.start_simulation()
+print("start sim")
+
+while True:
+    time.sleep(1)
